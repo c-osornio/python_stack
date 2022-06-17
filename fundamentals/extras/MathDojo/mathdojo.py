767655ec0dbfb93a65e008fe5a73ceac3f12b99b
Charlie <c.alejandro.oso@gmail.com>
2022-06-16 23:30:15 -0700

    added more calls to test functions

--- fundamentals/extras/MathDojo/mathdojo.py
+++ fundamentals/extras/MathDojo/mathdojo.py
@@ -23,11 +23,12 @@ x = md.add(2).add(2, 5, 1).subtract(3,2).result
 print(x)  
 # should print 5
 # run each of the methods a few more times and check the result!
-
+# Write the add method and test it by calling it 3 times, with different numbers of arguments each time
+# Write the subtract method and test it by calling it 3 times, with different numbers of arguments each time
 prob1 = MathDojo()
-y = prob1.add(100).add(-8, 20, 1).subtract(-87,250).result
+y = prob1.add(100).add(-8, 20, 1).add(0).subtract(-87,250).result
 print(y)  
 
 prob2 = MathDojo()
-z = prob2.add(77).add(10005, 852, 115).subtract(11049).result
+z = prob2.add(77).add(10005, 852, 115).subtract(88, 98).subtract(568,1,-87).subtract(10381).result
 print(z)  
