99acf8aacde22321b1eadcc4d8368a87d6187a99
Charlie <c.alejandro.oso@gmail.com>
2022-06-16 21:40:50 -0700

    submit

--- fundamentals/oop/deck_of_cards/game.py
+++ fundamentals/oop/deck_of_cards/game.py
@@ -1,21 +1,20 @@
-import classes.deck
-import classes.player
-import classes.card
-import classes.war
+import deck
+import player
+import war
 
 # Deck is created, shows cards and shuffles
-bicycle = classes.deck.Deck()
+bicycle = deck.Deck()
 # bicycle.show_cards()
 bicycle.shuffle_deck()
 # player 1 and 2 are created with empty hands
-player1 = classes.player.Player("Charlie")
-player2 = classes.player.Player("Aidee")
+player1 = player.Player.is_valid("Charlie")
+player2 = player.Player.is_valid("Aidee")
 # both players are delt half of the deck each
 player1.deal(bicycle)
 player2.deal(bicycle)
 # Players battle it out. Each players shows their top card and the player showing
 # the higher point_val wins the battle. Players battle until all cards are used.
 # Player with the most won battles wins the war! *Aces point_value = 14. 
-classes.war.playWar(player1, player2)
+war.playWar(player1, player2)
 # Deck is shuffled for a new game
 bicycle.shuffle_deck()
