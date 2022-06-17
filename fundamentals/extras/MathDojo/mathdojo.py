0517e2fc9d41963751e134c05f8d8f8b41080280
Charlie <c.alejandro.oso@gmail.com>
2022-06-16 23:26:48 -0700

    assignment complete

---
+++ fundamentals/extras/MathDojo/mathdojo.py
@@ -0,0 +1,33 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num, *nums):
+        self.result += num
+        for number in nums:
+            self.result += number
+        print(f'Answer: {self.result}')
+        return self
+
+    def subtract(self, num, *nums):
+        self.result -= num
+        for number in nums:
+            self.result -= number
+        print(f'Answer: {self.result}')
+        return self
+
+# create an instance:
+md = MathDojo()
+# to test:
+x = md.add(2).add(2, 5, 1).subtract(3,2).result
+print(x)  
+# should print 5
+# run each of the methods a few more times and check the result!
+
+prob1 = MathDojo()
+y = prob1.add(100).add(-8, 20, 1).subtract(-87,250).result
+print(y)  
+
+prob2 = MathDojo()
+z = prob2.add(77).add(10005, 852, 115).subtract(11049).result
+print(z)  
